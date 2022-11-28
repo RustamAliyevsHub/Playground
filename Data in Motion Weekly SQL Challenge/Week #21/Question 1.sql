cd6cd5ff8cba75cd151b29005466202c1b1b8bfd
Rustam Aliyev <110528857+RustamAliyevsHub@users.noreply.github.com>
2022-11-28 16:54:15 +0400

    Update Question 1.sql

--- Data in Motion Weekly SQL Challenge/Week #21/Question 1.sql
+++ Data in Motion Weekly SQL Challenge/Week #21/Question 1.sql
@@ -1,7 +1,7 @@
 --How many runners signed up for each 1 week period?
 
 SELECT
-	DATE_PART('week',registration_date) AS registration_week,
+    DATE_PART('week',registration_date) AS registration_week,
     COUNT(runner_id) AS new_registrations
 FROM pizza_runner.runners
 
